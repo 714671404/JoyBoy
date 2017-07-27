8f9b68f1b68ba87daf0152f6f1b188c9907af646
vagrant <vagrant@homestead>
2017-07-27 22:53:40 +0000

    User changes password

--- app/User.php
+++ app/User.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Naux\Mail\SendCloudTemplate;
+use Mail;
 
 class User extends Authenticatable
 {
@@ -26,4 +28,30 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /*
+     * 这个方法负责发送邮件 并且覆盖 trait注入的methdos
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        /*
+                 * $data变量：参数1：给定url路径并且传入token
+                 *            参数2：用户name
+                 */
+        $data = ['url' => url('password/reset', $token)];
+        /*
+         * $template变量：是SendCloudTemplate类的实例化对象 类默认传入参数，模板名字、 $data
+         */
+        $template = new SendCloudTemplate('JoyBoy_reset', $data);
+
+        /*
+         * Mail::raw传递template并回调函数，回调函数内，from方法写入发送邮箱的邮箱号，并备注title
+         *       to方法传入用户注册的邮箱地址
+         */
+        Mail::raw($template, function ($message) {
+            $message->from('714671404@qq.com', 'JoyBoy');
+
+            $message->to($this->email);
+        });
+    }
 }
